0c33cabdde080a9c360acf7c15ad8b59f7bd8677
chiu <80095372+mingchiuli@users.noreply.github.com>
2024-12-12 18:30:01 +0800

    Update AnnoValidator.java

--- src/main/java/com/example/demo/pkg/AnnoValidator.java
+++ src/main/java/com/example/demo/pkg/AnnoValidator.java
@@ -1,13 +1,15 @@
 package com.example.demo.pkg;
 
-import jakarta.annotation.Resource;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class AnnoValidator implements ConstraintValidator<Anno, Pojo>  {
 
-    @Resource
-    private TestComponent testComponent;
+    private final TestComponent testComponent;
+
+	public AnnoValidator(TestComponent testComponent) {
+		this.testComponent = testComponent;
+	}
 
     @Override
     public boolean isValid(Pojo value, ConstraintValidatorContext context) {
